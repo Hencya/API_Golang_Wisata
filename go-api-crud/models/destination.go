8a7761e29328e5c2c37ad3868be468750dea4885
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-05-02 10:42:16 +0700

    Add files via upload
    auto logout when not used for 1 hour

---
+++ go-api-crud/models/destination.go
@@ -0,0 +1,16 @@
+package models
+
+//User represents users table in database
+type Destination struct {
+	ID          uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	Title       string `gorm:"type:varchar(50)" json:"title"`
+	Description string `gorm:"type:text" json:"description"`
+	Keyword     string `gorm:"type:varchar(25)" json:"keyword"`
+	Images      string `gorm:"type:varchar(255)" json:"images"`
+	Address     string `gorm:"type:varchar(255)" json:"address"`
+	Views       uint64 `gorm:"type:int" json:"views"`
+
+	//jika si user mengambil ke destinasi
+	UserID uint64 `gorm:"not null" json:"-"`
+	User   User   `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
+}
---
+++ go-api-crud/service/jwt-service.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+//JWTService is a contract of what jwtService can do
+type JWTService interface {
+	GenerateToken(userID string) string
+	ValidateToken(token string) (*jwt.Token, error)
+}
+
+type jwtCustomClaim struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+type jwtService struct {
+	secretKey string
+	issuer    string
+}
+
+//NewJWTService method is creates a new instance of JWTService
+func NewJWTService() JWTService {
+	return &jwtService{
+		issuer:    "hencya",
+		secretKey: getSecretKey(),
+	}
+}
+
+func getSecretKey() string {
+	secretKey := os.Getenv("JWT_SECRET")
+	if secretKey != "" {
+		secretKey = "hencya"
+	}
+	return secretKey
+}
+
+func (j *jwtService) GenerateToken(UserID string) string {
+	claims := &jwtCustomClaim{
+		UserID,
+		jwt.StandardClaims{
+			//auto logout in 1 hour if not being used
+			ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
+			Issuer:    j.issuer,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(j.secretKey))
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func (j *jwtService) ValidateToken(token string) (*jwt.Token, error) {
+	return jwt.Parse(token, func(t_ *jwt.Token) (interface{}, error) {
+		if _, ok := t_.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method %v", t_.Header["alg"])
+		}
+		return []byte(j.secretKey), nil
+	})
+}
